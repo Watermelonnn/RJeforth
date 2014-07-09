ffcce002c7886b71beb75ed2dab6f2ca9d7c247e
Daniel Teng <u8505154@gmail.com>
2014-07-09 09:31:33 +0800

    Add some functions

--- rJeforth/app.js
+++ rJeforth/app.js
@@ -8,14 +8,15 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , ejs = require('ejs')
+  , vis = require('./routes/vis')	// 2014.07.08 加入連結 RIO 的測試程式
   , path = require('path');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
+app.engine('.html', ejs.__express);
 app.set('views', __dirname + '/views');
-app.engine('.html', 'ejs.__express');
 app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -31,7 +32,10 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/vis/rio', vis.rio);  // 2014.07.08 加入連結 RIO 的測試程式
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  // console.log('Express server listening on port ' + app.get('port'));
+  console.log('網頁服務器執行中。');
+  console.log('服務器正在監聽 ' + app.get('port') + ' 端口');
 });
--- rJeforth/routes/index.js
+++ rJeforth/routes/index.js
@@ -4,5 +4,11 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+	{ 
+		 title : 'rJeForth 測試環境',
+		Author : '臺灣符式推廣協會',
+		  Date : '2014-07-08' 
+	}
+  );
 };
